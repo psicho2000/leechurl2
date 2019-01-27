6ee312cbcc8a14a68e72e545ab035fb0027f4e6c
Markus Meier <markus.d.meier@gmail.com>
2019-01-27 21:51:06 +0100

    add Google Prediction (using AutoML Vision Api)

--- src/main/java/de/psicho/LeechUrl/Main.java
+++ src/main/java/de/psicho/LeechUrl/Main.java
@@ -2,11 +2,9 @@ package de.psicho.LeechUrl;
 
 import java.io.IOException;
 
-import static org.apache.commons.lang3.ArrayUtils.addAll;
-
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        PredictionApi.argsHelper(addAll(args, "sort-pics", "us-central1"), System.out);
+        PredictionApi.predict("abstract_headphones.jpg");
     }
 }
--- src/main/java/de/psicho/LeechUrl/PredictionApi.java
+++ src/main/java/de/psicho/LeechUrl/PredictionApi.java
@@ -1,12 +1,9 @@
 /*
  * Copyright 2018 Google Inc.
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
  * http://www.apache.org/licenses/LICENSE-2.0
- *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,7 +14,6 @@
 /*
  * This application demonstrates how to perform basic operations on prediction
  * with the Google AutoML Vision API.
- *
  * For more information, the documentation at
  * https://cloud.google.com/vision/automl/docs.
  */
@@ -26,110 +22,105 @@ package de.psicho.LeechUrl;
 
 // Imports the Google Cloud client library
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.channels.Channels;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.io.IOUtils;
+
+import com.google.auth.Credentials;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.ReadChannel;
 import com.google.cloud.automl.v1beta1.AnnotationPayload;
 import com.google.cloud.automl.v1beta1.ExamplePayload;
 import com.google.cloud.automl.v1beta1.Image;
 import com.google.cloud.automl.v1beta1.ModelName;
 import com.google.cloud.automl.v1beta1.PredictResponse;
 import com.google.cloud.automl.v1beta1.PredictionServiceClient;
+import com.google.cloud.automl.v1beta1.PredictionServiceSettings;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import com.google.common.collect.Lists;
 import com.google.protobuf.ByteString;
-import net.sourceforge.argparse4j.ArgumentParsers;
-import net.sourceforge.argparse4j.inf.ArgumentParser;
-import net.sourceforge.argparse4j.inf.ArgumentParserException;
-import net.sourceforge.argparse4j.inf.Namespace;
-import net.sourceforge.argparse4j.inf.Subparser;
-import net.sourceforge.argparse4j.inf.Subparsers;
-
-import java.io.IOException;
-import java.io.PrintStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @see <a href="https://github.com/GoogleCloudPlatform/java-docs-samples/tree/master/vision/automl">Java Sample by Google</a>
- * <p>
- * Google Cloud AutoML Vision API sample application. Example usage: mvn package exec:java
- * -Dexec.mainClass ='main.java.de.psicho.LeechUrl.PredictionApi' -Dexec.args='predict
- * [modelId] [path-to-image] [scoreThreshold]'
  */
 public class PredictionApi {
+
+    private static final String BUCKET_NAME = "sort-pics-vcm";
+    private static final String PROJECT_ID = "sort-pics";
+    private static final String COMPUTE_REGION = "us-central1";
+    private static final String MODEL_ID = "ICN4029841784534137370";
+    private static final String SCORE_THRESHOLD = "0.8";
+    private static final String CREDENTIAL_PATH = "D:\\Eigenes\\Desktop\\sort-pics-9f5bbbe58175.json";
+    private static final Credentials CREDENTIALS = createCredentials();
 
     /**
      * Demonstrates using the AutoML client to predict an image.
      *
-     * @param projectId      the Id of the project.
-     * @param computeRegion  the Region name.
-     * @param modelId        the Id of the model which will be used for text classification.
-     * @param filePath       the Local text file path of the content to be classified.
-     * @param scoreThreshold the Confidence score. Only classifications with confidence score above
-     *                       scoreThreshold are displayed.
+     * @param filename the Local text file path of the content to be classified.
      * @throws IOException on Input/Output errors.
      */
-    public static void predict(
-            String projectId,
-            String computeRegion,
-            String modelId,
-            String filePath,
-            String scoreThreshold)
-            throws IOException {
+    public static void predict(String filename) throws IOException {
 
         // Instantiate client for prediction service.
-        PredictionServiceClient predictionClient = PredictionServiceClient.create();
+        PredictionServiceSettings predictionServiceSettings =
+            PredictionServiceSettings.newBuilder().setCredentialsProvider(() -> CREDENTIALS).build();
+        PredictionServiceClient predictionClient = PredictionServiceClient.create(predictionServiceSettings);
 
         // Get the full path of the model.
-        ModelName name = ModelName.of(projectId, computeRegion, modelId);
-
-        // Read the image and assign to payload.
-        ByteString content = ByteString.copyFrom(Files.readAllBytes(Paths.get(filePath)));
-        Image image = Image.newBuilder().setImageBytes(content).build();
-        ExamplePayload examplePayload = ExamplePayload.newBuilder().setImage(image).build();
+        ModelName name = ModelName.of(PROJECT_ID, COMPUTE_REGION, MODEL_ID);
 
         // Additional parameters that can be provided for prediction e.g. Score Threshold
         Map<String, String> params = new HashMap<>();
-        if (scoreThreshold != null) {
-            params.put("score_threshold", scoreThreshold);
-        }
+        params.put("score_threshold", SCORE_THRESHOLD);
+
+        // FIXME: foreach or rather use batch mode
+        // https://cloud.google.com/ml-engine/docs/tensorflow/batch-predict
+        // https://cloud.google.com/ml-engine/reference/rest/v1/projects.jobs#PredictionInput
+
+        // Read the image and assign to payload.
+        ByteString content = getFileFromBucket(filename);
+        Image image = Image.newBuilder().setImageBytes(content).build();
+        ExamplePayload examplePayload = ExamplePayload.newBuilder().setImage(image).build();
+
         // Perform the AutoML Prediction request
         PredictResponse response = predictionClient.predict(name, examplePayload, params);
 
         System.out.println("Prediction results:");
         for (AnnotationPayload annotationPayload : response.getPayloadList()) {
             System.out.println("Predicted class name :" + annotationPayload.getDisplayName());
-            System.out.println(
-                    "Predicted class score :" + annotationPayload.getClassification().getScore());
+            System.out.println("Predicted class score :" + annotationPayload.getClassification().getScore());
         }
     }
 
-    public static void argsHelper(String[] args, PrintStream out) throws IOException {
-        ArgumentParser parser =
-                ArgumentParsers.newFor("PredictionApi")
-                        .build()
-                        .defaultHelp(true)
-                        .description("Prediction API Operation");
-        Subparsers subparsers = parser.addSubparsers().dest("command");
-
-        Subparser predictParser = subparsers.addParser("predict");
-        predictParser.addArgument("modelId");
-        predictParser.addArgument("filePath");
-        predictParser.addArgument("scoreThreshold").nargs("?").type(String.class).setDefault("");
-        predictParser.addArgument("projectId");
-        predictParser.addArgument("regionName");
-
-        Namespace ns;
+    private static Credentials createCredentials() {
+        GoogleCredentials credentials = null;
         try {
-            ns = parser.parseArgs(args);
-            if (ns.get("command").equals("predict")) {
-                predict(
-                        ns.getString("projectId"),
-                        ns.getString("regionName"),
-                        ns.getString("modelId"),
-                        ns.getString("filePath"),
-                        ns.getString("scoreThreshold"));
-            }
-        } catch (ArgumentParserException e) {
-            parser.handleError(e);
+            credentials = GoogleCredentials.fromStream(new FileInputStream(CREDENTIAL_PATH))
+                .createScoped(Lists.newArrayList("https://www.googleapis.com/auth/cloud-platform"));
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
+        return credentials;
+    }
+
+    public static ByteString getFileFromBucket(String filename) {
+        String basePath = "_unrated";
+        String filePath = basePath + "/" + filename;
+
+        Storage storage = StorageOptions.newBuilder().setCredentials(CREDENTIALS).build().getService();
+
+        try (ReadChannel reader = storage.reader(BUCKET_NAME, filePath)) {
+            InputStream inputStream = Channels.newInputStream(reader);
+            return ByteString.copyFrom(IOUtils.toByteArray(inputStream));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return null;
     }
 }
